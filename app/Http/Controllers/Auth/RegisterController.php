187d853b5dc5e4cad1b4199117709c4ba508ff10
henneth <henneth.cheng@gmail.com>
2018-12-02 01:33:10 +0800

    t

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -62,7 +62,6 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        echo $data['email'];
         return User::create([
             'username' => $data['name'],
             'email' => $data['email'],
