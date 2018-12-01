4ea735c04690d210f48c9744b48d7b54a77039e9
henneth <henneth.cheng@gmail.com>
2018-12-02 01:37:43 +0800

    t

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -65,7 +65,7 @@ class RegisterController extends Controller
         return User::create([
             'username' => $data['name'],
             'email' => $data['email'],
-            'password' => bcrypt($data['password']),
+            'password' => $data['password'],
         ]);
     }
 }
